3bb464bb7616cff18b1839cc8a438ea9fec6a808
ThanizeR <50957189+ThanizeR@users.noreply.github.com>
2024-11-22 10:19:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1136,7 +1136,7 @@ elif selected == "🌱Pulverizadores":
                     # Verificar se a coluna "Fator de Carga Média do Motor (Ag) Trabalho (%)" está presente
                     if "Fator de Carga Médio do Motor Trabalho (%)" in df_selected_tractors_fator.columns:
                         # Ordenar o DataFrame com base no "Fator de Carga Média do Motor (Ag) Trabalho (%)" de forma decrescente
-                        df_selected_tractors_fator = df_selected_tractors_fator.sort_values(by="Fator de Carga Médio do Motor Trabalho (%)", ascending=True)
+                        df_selected_tractors_fator = df_selected_tractors_fator.sort_values(by="Fator de Carga Médio do Motor Trabalho (%)", ascending=False)
                     # Reverter a ordem para o gráfico, de modo que o maior valor apareça no topo
                     df_selected_tractors_fator = df_selected_tractors_fator[::-1]
 
